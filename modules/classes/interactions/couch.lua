d6cd95ce94475866cb63156419c87c53473d3519
keanuWheeze <48886873+justarandomguyintheinternet@users.noreply.github.com>
2025-04-28 21:30:42 +0200

    Couch sit types and TV controls

--- modules/classes/interactions/couch.lua
+++ modules/classes/interactions/couch.lua
@@ -5,7 +5,10 @@ local workspot = require("modules/classes/interactions/workspot")
 ---Class for couch interaction
 ---@class couch : workspot
 ---@field maxNodeRefPropertyWidth number?
+---@field maxActionPropertyWidth number?
 ---@field tvRef string
+---@field enableTVControls boolean
+---@field sitType number
 local couch = setmetatable({}, { __index = workspot })
 
 function couch:new(mod, project)
@@ -27,10 +30,20 @@ function couch:new(mod, project)
     o.editorIcon = IconGlyphs.Sofa
 
     o.maxNodeRefPropertyWidth = nil
+    o.maxActionPropertyWidth = nil
     o.tvRef = ""
+    o.enableTVControls = false
+    o.sitType = 1
 
     setmetatable(o, { __index = self })
    	return o
+end
+
+function couch:load(data)
+    workspot.load(self, data)
+
+    CName.add("nif_tv_controls")
+    CName.add("nif_sit_type")
 end
 
 function couch:getPatchData()
@@ -43,8 +56,41 @@ function couch:getPatchData()
     return data
 end
 
+function couch:onUpdate()
+    if self.sceneRunning then
+        Game.GetQuestsSystem():SetFact("nif_tv_controls", self.enableTVControls and 1 or 0)
+        Game.GetQuestsSystem():SetFact("nif_sit_type", self.sitType)
+    end
+end
+
 function couch:draw()
     workspot.draw(self)
+
+    style.sectionHeaderStart("ACTIONS")
+
+    if not self.maxActionPropertyWidth then
+        self.maxActionPropertyWidth = utils.getTextMaxWidth({ "Sit Animation", "Enable TV Controls" }) + 4 * ImGui.GetStyle().ItemSpacing.x
+    end
+
+    style.mutedText("Sit Animation:")
+    ImGui.SameLine()
+    ImGui.SetCursorPosX(self.maxActionPropertyWidth)
+    style.setNextItemWidth(300)
+    local sitType, changed = ImGui.Combo("##sitType", self.sitType - 1, { "Legs Normal", "Legs Wide", "Legs Crossed" }, 3)
+    if changed then
+        self.sitType = sitType + 1
+        self.project:save()
+    end
+
+    style.mutedText("Enable TV Controls:")
+    ImGui.SameLine()
+    ImGui.SetCursorPosX(self.maxActionPropertyWidth)
+    self.enableTVControls, changed = ImGui.Checkbox('##enableTVControls', self.enableTVControls)
+    if changed then self.project:save() end
+
+    style.sectionHeaderEnd()
+
+    if not self.enableTVControls then return end
 
     style.sectionHeaderStart("PROPS")
 
@@ -68,6 +114,8 @@ function couch:save()
     local data = workspot.save(self)
 
     data.tvRef = self.tvRef
+    data.enableTVControls = self.enableTVControls
+    data.sitType = self.sitType
 
     return data
 end

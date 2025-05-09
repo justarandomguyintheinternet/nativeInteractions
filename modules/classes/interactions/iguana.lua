a098464868f0321bb7f6b9edd06c3a727c84e911
keanuWheeze <48886873+justarandomguyintheinternet@users.noreply.github.com>
2025-05-09 15:28:06 +0200

    Iguana setup

---
+++ modules/classes/interactions/iguana.lua
@@ -0,0 +1,109 @@
+local style = require("modules/ui/style")
+local utils = require("modules/utils/utils")
+local world = require("modules/utils/worldInteraction")
+local resourceHelper = require("modules/utils/resourceHelper")
+local workspot = require("modules/classes/interactions/workspot")
+
+---Class for iguana interaction
+---@class iguana : workspot
+---@field maxNodeRefPropertyWidth number?
+---@field iguanaRef string
+---@field animationDistance number
+local iguana = setmetatable({}, { __index = workspot })
+
+function iguana:new(mod, project)
+    ---@class iguana
+	local o = workspot.new(self, mod, project)
+
+    o.interactionType = "Iguana"
+    o.modulePath = "interactions/iguana"
+    o.scene = "nif\\quest\\iguana.scene"
+    o.skipFact = "nif_skip_iguana"
+    o.endEvent = "nif_exit_iguana"
+    o.startFactID = 18
+
+    o.name = "Incense Interaction"
+    o.worldIcon = "ChoiceIcons.UseIcon"
+    o.worldIconRange = 5
+    o.interactionAngle = 80
+    o.interactionRange = 1.5
+    o.editorIcon = IconGlyphs.Torch
+
+    o.maxNodeRefPropertyWidth = nil
+    o.iguanaRef = ""
+    o.animationDistance = 8
+
+    o.animationActive = false
+    o.choiceActive = false
+
+    setmetatable(o, { __index = self })
+   	return o
+end
+
+function iguana:getPatchData()
+    local data = workspot.getPatchData(self)
+
+    data.propMap = {
+        [utils.nodeRefStringToHashString("$/nif_iguana")] = self.iguanaRef
+    }
+
+    return data
+end
+
+function iguana:start()
+    if resourceHelper.endEvents[self.endEvent] and not self.animationActive then return end
+
+    -- set fact to jump to choice hub
+end
+
+function iguana:stop()
+    if not self.sceneRunning or (resourceHelper.endEvents[self.endEvent] and not self.animationActive) then return end
+
+    -- set fact to close choice hub
+end
+
+function iguana:onUpdate()
+    -- Reset incense if far away
+    local distance = GetPlayer():GetWorldPosition():Distance(ToVector4(self.worldIconPosition))
+    if distance < animationDistance and not self.animationActive and not resourceHelper.endEvents[self.endEvent] then
+        workspot.start(self)
+        self.animationActive = true
+    elseif distance > animationDistance and self.animationActive then
+        workspot.stop(self)
+        self.animationActive = false
+    end
+end
+
+function iguana:draw()
+    workspot.draw(self)
+
+    style.sectionHeaderStart("IGUANA")
+
+    if not self.maxNodeRefPropertyWidth then
+        self.maxNodeRefPropertyWidth = utils.getTextMaxWidth({ "Iguana" }) + 4 * ImGui.GetStyle().ItemSpacing.x
+    end
+
+    style.mutedText("Iguana:")
+    ImGui.SameLine()
+    ImGui.SetCursorPosX(self.maxNodeRefPropertyWidth)
+    style.setNextItemWidth(300)
+    self.iguanaRef, changed = ImGui.InputTextWithHint('##iguanaRef', '$/mod/#iguana', self.iguanaRef, 250)
+    if changed then self.project:save() end
+    if ImGui.IsItemDeactivatedAfterEdit() then
+        self:reset()
+    end
+    ImGui.SameLine()
+    style.drawNodeRefInfo(self.iguanaRef, true)
+
+    style.sectionHeaderEnd()
+end
+
+function iguana:save()
+    local data = workspot.save(self)
+
+    data.iguanaRef = self.iguanaRef
+
+    return data
+end
+
+return iguana

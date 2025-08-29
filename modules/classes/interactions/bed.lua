4f10a01b5a2aeb2f6a97799d770e7460206f7dc5
keanuWheeze <leon.frensch@gmail.com>
2025-08-29 11:12:24 +0200

    Bed settings code

--- modules/classes/interactions/bed.lua
+++ modules/classes/interactions/bed.lua
@@ -1,7 +1,9 @@
+local style = require("modules/ui/style")
 local workspot = require("modules/classes/interactions/workspot")
 
 ---Class for bed interaction
 ---@class bed : workspot
+---@field allowBedrot boolean
 local bed = setmetatable({}, { __index = workspot })
 
 function bed:new(mod, project)
@@ -22,8 +24,39 @@ function bed:new(mod, project)
     o.interactionRange = 1.5
     o.editorIcon = IconGlyphs.BedOutline
 
+    o.allowBedrot = false
+
     setmetatable(o, { __index = self })
    	return o
+end
+
+function bed:start()
+    if not self.sceneRunning then
+        Game.GetQuestsSystem():SetFactStr("nif_bed_stay", self.allowBedrot and 1 or 0)
+    end
+
+    workspot.start(self)
+end
+
+function bed:draw()
+    workspot.draw(self)
+
+    style.sectionHeaderStart("ACTIONS")
+
+    style.mutedText("Enable Staying In Bed:")
+    ImGui.SameLine()
+    self.allowBedrot, changed = ImGui.Checkbox('##allowBedrot', self.allowBedrot)
+    if changed then self.project:save() end
+
+    style.sectionHeaderEnd()
+end
+
+function bed:save()
+    local data = workspot.save(self)
+
+    data.allowBedrot = self.allowBedrot
+
+    return data
 end
 
 return bed
